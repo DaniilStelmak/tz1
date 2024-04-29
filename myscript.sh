e5d90875e0a6daa53b2de60887803c9c284879c3
DaniilStelmak <154973215+DaniilStelmak@users.noreply.github.com>
2024-04-29 19:11:36 +0300

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,28 +1,44 @@
 #!/bin/bash
 
+# Принять обе директории как аргументы
 input_dir="$1"
 output_dir="$2"
 
+# Создание выходной директорию, если она не существует
 mkdir -p "$output_dir"
 
 function copy_files {
+    # Перебор всех элементов в текущей директории
     for file in "$1"/*; do
         if [[ -d "$file" ]]; then
+            # Рекурсивно вызвать функцию если элемент является директорией
             copy_files "$file"
         elif [[ -f "$file" ]]; then
-            new_file="$output_dir/$(basename "$file")"
+            # Определить базовое имя и расширение файла
+            base=$(basename "$file")
+            ext=""
+
+            if [[ "$base" == *.* ]]; then
+                ext=".${base##*.}"
+                base="${base%.*}"
+            fi
+
+            new_file="$output_dir/${base}${ext}"
+
+            # Решение проблемы одинаковых имен файлов в выходной директории
             if [[ -f "$new_file" ]]; then
-                base="${file%.*}"
-                ext="${file##*.}"
                 i=1
-                while [[ -f "$new_file" ]]; do
-                    new_file="$output_dir/$(basename "$base")_$i.$ext"
+                while [[ -f "$output_dir/${base}_${i}${ext}" ]]; do
                     let i++
                 done
+                new_file="$output_dir/${base}_${i}${ext}"
             fi
+
+            # Копирование файла из входной директории в выходную
             cp "$file" "$new_file"
         fi
     done
 }
 
+# Начальный вызов функции с входной директорией
 copy_files "$input_dir"
