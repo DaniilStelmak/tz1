7a9f80b5170d76042f4e1e3609dd02c32f9e48ac
DaniilStelmak <154973215+DaniilStelmak@users.noreply.github.com>
2024-05-02 18:21:10 +0300

    Update myscript.sh

--- myscript.sh
+++ myscript.sh
@@ -1,44 +1,51 @@
 #!/bin/bash
 
-# Принять обе директории как аргументы
 input_dir="$1"
 output_dir="$2"
 
-# Создание выходной директорию, если она не существует
+# Создаём выходную директорию, если она не существует
 mkdir -p "$output_dir"
 
 function copy_files {
-    # Перебор всех элементов в текущей директории
-    for file in "$1"/*; do
-        if [[ -d "$file" ]]; then
-            # Рекурсивно вызвать функцию если элемент является директорией
-            copy_files "$file"
-        elif [[ -f "$file" ]]; then
-            # Определить базовое имя и расширение файла
-            base=$(basename "$file")
-            ext=""
+    # Перебор всех элементов в текущей директории (включая скрытые)
+    for file in "$1"/* "$1"/.*; do
+        # Исключить текущую и родительскую директории
+        if [[ "$file" != "$1/." && "$file" != "$1/.." ]]; then
+            if [[ -d "$file" && -r "$file" ]]; then
+                # Рекурсивно копировать если это директория и доступна для чтения
+                copy_files "$file"
+            elif [[ -f "$file" || -L "$file" && -r "$file" ]]; then
+                base=$(basename "$file")
+                ext=""
 
-            if [[ "$base" == *.* ]]; then
-                ext=".${base##*.}"
-                base="${base%.*}"
+                if [[ "$base" == *.* ]]; then
+                    ext=".${base##*.}"
+                    base="${base%.*}"
+                fi
+
+                new_file="$output_dir/${base}${ext}"
+
+                # Решение конфликта одинаковых имен файлов
+                if [[ -e "$new_file" ]]; then
+                    i=1
+                    while [[ -e "$output_dir/${base}_${i}${ext}" ]]; do
+                        let i++
+                    done
+                    new_file="$output_dir/${base}_${i}${ext}"
+                fi
+
+                # Копировать файл или ссылку
+                if [[ -L "$file" ]]; then
+                    # Копировать ссылку
+                    cp -d "$file" "$new_file"
+                else
+                    # Копировать файл
+                    cp "$file" "$new_file"
+                fi
             fi
-
-            new_file="$output_dir/${base}${ext}"
-
-            # Решение проблемы одинаковых имен файлов в выходной директории
-            if [[ -f "$new_file" ]]; then
-                i=1
-                while [[ -f "$output_dir/${base}_${i}${ext}" ]]; do
-                    let i++
-                done
-                new_file="$output_dir/${base}_${i}${ext}"
-            fi
-
-            # Копирование файла из входной директории в выходную
-            cp "$file" "$new_file"
         fi
     done
 }
 
-# Начальный вызов функции с входной директорией
+# Начальный вызов функции
 copy_files "$input_dir"
